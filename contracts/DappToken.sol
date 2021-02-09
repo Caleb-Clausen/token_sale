b45ab78bc2f970310334a436793b2a9966a28db5
Caleb Clausen <Caleb.Clausen@outlook.com>
2021-02-09 19:20:48 +0100

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -4,17 +4,38 @@ pragma solidity ^0.5.0;
  * The DappToken contract does this and that...
  */
 contract DappToken {
-	// Constructor
-	// Set total number of tokens
-	// Read total number of tokens
+	string public name = 'DApp Token';
+	string public symbol = "DAPP";
+	string public standard = "DApp Token v1.0";
 	uint256 public totalSupply;
 
-	constructor () public {
-		totalSupply = 1000000;
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
 
+		);
+	
 
+	mapping (address => uint256) public balanceOf;
+	
+
+	constructor (uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply; // msg.sender is the address that is deploying the contract
+		totalSupply = _initialSupply;
+		//allocate the initial supply
 	}
 
-
-
+	// Transfer
+	function transfer(address _to, uint256 _value) public returns (bool success){
+	// Exception if account does not have enough 
+	require(balanceOf[msg.sender] >= _value); // msg.sender address who is call this function, checking his balance
+	// Transfer the Balance
+	balanceOf[msg.sender] -= _value;
+	balanceOf[_to] += _value;
+	// Transfer Event
+	emit Transfer(msg.sender, _to, _value);	
+	// Return a boolean
+	return true;
+	}
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@
 const DappToken = artifacts.require("DappToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(DappToken);
+  deployer.deploy(DappToken, 1000000); // 1000000 is an argument that is used by the constructor
 };
 
 
